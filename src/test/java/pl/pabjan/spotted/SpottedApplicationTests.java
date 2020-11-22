cb57400b0a96db58a6375838b8176b4f08ba496a
ArturPab <artur.pabjan00@gmail.com>
2020-11-22 17:24:29 +0100

    Create new spotted project

---
+++ src/test/java/pl/pabjan/spotted/SpottedApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pabjan.spotted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpottedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
